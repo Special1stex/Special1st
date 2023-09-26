22727e60baac668a03722b1568ca85ac4f2152e6
Special1stex <143091604+Special1stex@users.noreply.github.com>
2023-09-26 15:13:12 +0700

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,2 +1,5 @@
 print("Server Info Successfully Refreshed")
-warn("Server Info : 244+ms, Singapore, FFA Mode")
+warn("Server Info : 244+ms, Singapore, PAID Mode")
+warn("NOT BUYER")
+
+game.LocalPlayer:Kick("The script has been disabled, please try again later. | アトミック ")
